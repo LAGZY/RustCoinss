85d81dc75657f3980ea92b3ddd3e0392a25b9db8
лагзя <lagzya@gmail.com>
2022-05-01 18:02:30 +0300

    NewPlugin
    Signed-off-by: лагзя <lagzya@gmail.com>

---
+++ RustCoinProject/MonumentsMarkers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Facepunch;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("MonumentsMarkers", "LAGZYA", "1.0.0")]
+    public class MonumentsMarkers : RustPlugin
+    {
+        private static MonumentsMarkers ins;
+        private GameObject objecGameObject = new GameObject();
+
+        void OnServerInitialized()
+        {
+            ins = this;
+            objecGameObject = new GameObject();
+        }
+
+        [ChatCommand("testmarker")]
+        void Marker(BasePlayer player)
+        {
+            var marker = objecGameObject.AddComponent<MonumentMarker>();
+            var land = new LandmarkInfo();
+            land.transform.position = player.transform.position;
+            marker.Setup(land);
+            marker.text.text = "0";
+            marker.gameObject.AddComponent<TestMarker>();
+        }
+
+        class TestMarker : MonoBehaviour
+        {
+            private int text = 1;
+            private MonumentMarker t;
+
+            private void Awake()
+            {
+                t = GetComponent<MonumentMarker>();
+            }
+
+            private void FixedUpdate()
+            {
+                if (!ins.IsLoaded)
+                {
+                    Destroy(this);
+                    t.SetActive(false);
+                    return;
+                }
+
+                t.text.text = text.ToString();
+                text++;
+            }
+        }
+    }
+}
